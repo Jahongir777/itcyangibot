16935ca6bdcde9e8e65c4aaca72490b3f18dcbc5
Jahongir <xudoyberganovjahongir7@gmail.com>
2021-10-30 11:40:53 -0700

    thrid commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const { Telegraf, Markup, Scenes, session } = require('telegraf');
-const registrationScene = require('./scenes/registration.js')
+const {Telegraf, session, Scenes: {BaseScene, Stage}, Markup} = require('telegraf')
 require('dotenv').config();
 
 const token = process.env.BOT_TOKEN;
@@ -8,11 +7,8 @@ const bot = new Telegraf(token);
 
 const chatId = 1018120467;
 
-const stage = new Scenes.Stage([registrationScene])
-bot.use(session());
-bot.use(stage.middleware())
 
-bot.hears('📝 Ro\'yhatdan o\'tish', ctx => ctx.scene.enter('registrationWizard'))
+
 
 
 bot.start(async (ctx) => {
@@ -51,5 +47,80 @@ bot.hears('🔷 ITC-HUB haqida 🔷', async (ctx) => {
     return await ctx.reply('Itc-hub bu innovatsion o\'quv markaz hisoblanadi'
     )
   })
+
+  const nameScene = new BaseScene("nameScene");
+nameScene.enter((ctx) => ctx.reply("Ismingiz va familiyangiz"));
+nameScene.on("text", (ctx) => {
+  ctx.session.name = ctx.message.text;
+  return ctx.scene.enter("ageScene", { name: ctx.message.text });
+});
+
+const ageScene = new BaseScene("ageScene");
+ageScene.enter((ctx) => ctx.reply("Yoshingiz nechida?"));
+ageScene.on("text", (ctx) => {
+  ctx.session.age = ctx.message.text;
+  return ctx.scene.enter("courseScene", { age: ctx.message.text });
+});
+
+const courseScene = new BaseScene("courseScene");
+courseScene.enter((ctx) =>
+  ctx.reply(
+    "Qaysi yo'nalishda o'qishni hohlaysiz?\n (Misol uchun: Web dasturlash, Android dasturlash, Video mantaj, Kompyuter savodhonligi va h.k)"
+  )
+);
+courseScene.on("text", (ctx) => {
+  ctx.session.course = ctx.message.text;
+  return ctx.scene.enter("infoScene", { course: ctx.message.text });
+});
+
+const infoScene = new BaseScene("infoScene");
+infoScene.enter((ctx) =>
+  ctx.reply(
+    "Bu yo'nalish bo'yicha ma'lumotingiz qanday?\n(Misol uchun: Umuman bilmayman, o'rtacha, yaxshi bilaman"
+  )
+);
+infoScene.on("text", (ctx) => {
+  ctx.session.info = ctx.message.text;
+
+  ctx.reply(
+    `Anketa o\'rnatildi jo'natishni tasdiqlaysizmi?\n\n👨‍🎓 Ismi: ${ctx.session?.name}\n🧍 Yoshi: ${ctx.session?.age}\n💻 Tanlangan yo\'nalish: ${ctx.session?.course}\n📚 Ma\'lumoti: ${ctx.session?.info}`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Anketani jo'natish", callback_data: "send" }],
+        ],
+      },
+    }
+  );
+  return ctx.scene.leave();
+});
+
+//const bot = new Telegraf("TOKEN");
+//const chatId = 1018120467;
+const stage = new Stage([nameScene, ageScene, courseScene, infoScene]);
+stage.hears("exit", (ctx) => ctx.scene.leave());
+
+bot.use(session());
+bot.use(stage.middleware());
+bot.command("/start", (ctx) => {
+  return ctx.reply(
+    `Assalom alaykum ${ctx.message.from.first_name}!`,
+    Markup.keyboard([["📝 Ro\'yhatdan o\'tish"]]).resize()
+  );
+});
+bot.hears("📝 Ro\'yhatdan o\'tish", (ctx) => ctx.scene.enter("nameScene"));
+bot.command("/send", (ctx) =>
+  ctx.telegram.sendMessage(
+    chatId,
+    `Ism: ${ctx.session?.name}\nYoshi: ${ctx.session?.age}\nTanlangan yo\'nalish: ${ctx.session?.course}\nMa\'lumoti: ${ctx.session?.info}`
+  )
+);
+bot.action("send", (ctx) => {
+  return ctx.telegram.sendMessage(
+    chatId,
+    `Yangi o'quvchi\n\n👨‍🎓 Ismi: ${ctx.session?.name}\n🧍 Yoshi: ${ctx.session?.age}\n💻 Tanlangan yo\'nalish: ${ctx.session?.course}\n📚 Ma\'lumoti: ${ctx.session?.info}`
+  );
+});
+
 
 bot.launch();
